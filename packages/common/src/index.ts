31ec2cc211ae13427a750e756e43f057c27e18ab
pruthviraj <marganepruthviraj@gmail.com>
2025-04-27 23:06:08 +0530

    added schemas

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,14 +1,29 @@
 import { z } from "zod";
 
+export const seatSchema = z.object({
+  price: z.number(),
+  type: z.enum([
+    "REGULAR",
+    "PREMIUM",
+    "RECLINER",
+    "VIP",
+    "COUPLE",
+    "BALCONY",
+    "SOFA",
+    "LUXURY",
+  ]),
+  seatNumber: z.string(),
+});
+
 export const createEventSchema = z.object({
   name: z.string(),
   startTime: z.date(),
   endTime: z.date(),
   venue: z.string(),
-  location: z.string(),
+  cityId: z.string(),
   description: z.string(),
   bannerImageUrl: z.string(),
-  seats : z.any()
+  seats: z.array(seatSchema),
 });
 
 export const updateEventSchema = z.object({
@@ -16,9 +31,51 @@ export const updateEventSchema = z.object({
   startTime: z.date(),
   endTime: z.date(),
   venue: z.string(),
-  location: z.string(),
+  cityId: z.string(),
   description: z.string(),
   bannerImageUrl: z.string(),
-  seats : z.any()
+  seats: z.array(seatSchema),
 });
 
+export const RazorpayWebhookSchema = z.object({
+  entity: z.literal("event"),
+  account_id: z.string(),
+  event: z.string(),
+  contains: z.array(z.string()),
+  payload: z.object({
+    payment: z.object({
+      entity: z.object({
+        id: z.string(),
+        entity: z.literal("payment"),
+        amount: z.number(),
+        currency: z.string(),
+        status: z.string(),
+        order_id: z.string(),
+        invoice_id: z.string().nullable(),
+        international: z.boolean(),
+        method: z.string(),
+        amount_refunded: z.number(),
+        refund_status: z.string().nullable(),
+        captured: z.boolean(),
+        description: z.string(),
+        card_id: z.string(),
+        bank: z.string().nullable(),
+        wallet: z.string().nullable(),
+        vpa: z.string().nullable(),
+        email: z.string(),
+        contact: z.string(),
+        notes: z.object({
+          bookingId: z.string(),
+          userId: z.string(),
+          eventId: z.string(),
+          seatIds: z.array(z.string()),
+        }),
+        fee: z.number(),
+        tax: z.number(),
+        created_at: z.number(),
+      })
+    })
+  }),
+  created_at: z.number(),
+  webHookSecret: z.string()
+});
