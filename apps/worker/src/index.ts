91bb655e9a6f2f64c1e970635d02845e02c42849
pruthviraj <marganepruthviraj@gmail.com>
2025-11-15 22:02:56 +0530

    Worker Logic Changed - added cron job + other fixes

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,65 +1,70 @@
+import cron from "node-cron";
 import { prisma } from "@repo/db";
 
 async function clearUnProcessedBookings() {
   console.log("Running clearUnProcessedBookings job...");
 
-  const pendingBookings = await prisma.booking.findMany({
+  const now = new Date();
+
+  const expiredBookings = await prisma.booking.findMany({
     where: {
       status: "PENDING",
+      bookedSeats: {
+        some: {
+          seat: {
+            lockedUntil: { lt: now },
+          },
+        },
+      },
     },
     include: {
       bookedSeats: {
-        include : {
-            seat : true
+        include: {
+          seat: true,
         },
       },
     },
   });
 
-  const now = new Date();
+  for (const booking of expiredBookings) {
+    const seatIds = booking.bookedSeats.map((bs) => bs.seatId);
 
-  for (const booking of pendingBookings) {
-    const expired = booking.bookedSeats.some((bs) => {
-      return bs.seat.lockedUntil && bs.seat.lockedUntil < now;
-    });
-
-    if (expired) {
-      await prisma.$transaction(async (tx) => {
-        await tx.bookedSeat.deleteMany({
-          where: { bookingId: booking.id },
-        });
-
-        await tx.seat.updateMany({
-          where: {
-            bookedSeat: {
-                bookingId: booking.id,
-            },
-          },
-          data: {
-            lockedUntil: null,
-          },
-        });
-
-        await tx.payment.updateMany({
-          where: {
-            bookingId: booking.id,
-          },
-          data: {
-            status: "FAILED",
-          },
-        });
-
-        await tx.booking.update({
-          where: { id: booking.id },
-          data: {
-            status: "EXPIRED",
-          },
-        });
+    await prisma.$transaction(async (tx) => {
+      await tx.bookedSeat.deleteMany({
+        where: { bookingId: booking.id },
       });
 
-      console.log(`Cleared expired booking: ${booking.id}`);
-    }
+      await tx.seat.updateMany({
+        where: {
+          id: {
+            in: seatIds,
+          },
+        },
+        data: {
+          lockedUntil: null,
+        },
+      });
+
+      await tx.payment.updateMany({
+        where: {
+          bookingId: booking.id,
+          status: "PENDING",
+        },
+        data: {
+          status: "FAILED",
+        },
+      });
+
+      await tx.booking.update({
+        where: { id: booking.id },
+        data: {
+          status: "EXPIRED",
+        },
+      });
+    });
+
+    console.log(`Cleared expired booking: ${booking.id}`);
   }
 }
 
-setInterval(clearUnProcessedBookings, 5 * 60 * 1000);
+cron.schedule("*/5 * * * *", clearUnProcessedBookings);
